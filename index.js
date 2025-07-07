f0673ac3dcfc32c6e3de5a7587df387c7a160144
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-07-07 17:35:45 -0500

    basic api

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi mom")
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/api/health', (req, res) => {
+    res.json({ status: 'ok' });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
